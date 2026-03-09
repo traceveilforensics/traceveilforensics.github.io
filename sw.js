20a06b473be133909231cec0d250aaa4c273c915
felisaries <felisaries@kali>
2026-03-09 07:52:42 +0300

    Add PWA support (manifest + service worker)

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'traceveil-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/shared.js',
+  '/favicon.png',
+  '/LOGO_NAME_optimized.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
